bed618d760d408a28772b9fafe46c5c96f39ff6d
Alex Boges <87436065+alexbherval@users.noreply.github.com>
2023-03-14 01:36:08 -0300

    Atualização FormCadPessoa
    Atualização do placeholder cadpessoa

--- src/js/sidebars.js
+++ src/js/sidebars.js
@@ -1,10 +1,13 @@
-/* global bootstrap: false */
-(() => {
-  "use strict";
-  const tooltipTriggerList = Array.from(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  tooltipTriggerList.forEach((tooltipTriggerEl) => {
-    new bootstrap.Tooltip(tooltipTriggerEl);
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+    arrowParent.classList.toggle("showMenu");
   });
-})();
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
