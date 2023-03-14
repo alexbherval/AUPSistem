74fb7344987125a42ad54cfd7bda8502b605e187
Alex Boges <87436065+alexbherval@users.noreply.github.com>
2023-03-14 17:20:15 -0300

    Construindo AppCep

---
+++ src/js/appConsultaCep.js
@@ -0,0 +1,11 @@
+/*
+ *** Este App tem a função de consultar o Cep em base dados e inserir o conteúdo no form
+ *** Esta disponibilizado para toda a aplicação em tempo de execução
+ */
+
+/*Capturando o cep do form*/
+
+const cep = document.getElementById("cep");
+cep.addEventListener("blur", (e) => {});
+
+console.log(cep);
